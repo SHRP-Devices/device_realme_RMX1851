9bcc830805169f44ab6050c015c5a8b67bd12477
ahmedmoselhi <ahmedmoselhi55@gmail.com>
2020-03-17 22:33:01 +0000

    update tree

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,7 +15,6 @@
 #
 
 SHRP_PATH := device/realme/RMX1851
-
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
@@ -43,7 +42,7 @@ TARGET_BOARD_PLATFORM := sdm710
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno616
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xA90000 androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 androidboot.configfs=true androidboot.usbcontroller=a600000.dwc3 swiotlb=1 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xA90000 androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 androidboot.configfs=true androidboot.usbcontroller=a600000.dwc3 swiotlb=1 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 androidboot.vbmeta.avb_version=1.0 androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00008000
@@ -51,13 +50,18 @@ BOARD_RAMDISK_OFFSET     := 0x01000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_PREBUILT_KERNEL := $(SHRP_PATH)/prebuilt/Image.gz-dtb
+#TARGET_PREBUILT_KERNEL := $(SHRP_PATH)/prebuilt/Image.gz-dtb
 BOARD_PREBUILT_DTBOIMAGE := $(SHRP_PATH)/prebuilt/dtbo.img
 BOARD_INCLUDE_RECOVERY_DTBO := true
 BOARD_BOOTIMG_HEADER_VERSION := 1
-#BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
-#BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
-#BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+  TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+  TARGET_KERNEL_SOURCE := kernel/realme/sdm710
+  TARGET_KERNEL_CONFIG := RMX1851_defconfig
+endif
 
 # QCOM
 #TARGET_USE_SDCLANG := true
@@ -66,8 +70,7 @@ BOARD_BOOTIMG_HEADER_VERSION := 1
 TARGET_OTA_ASSERT_DEVICE := RMX1851
 
 # Avb
-BOARD_AVB_ENABLE := true
-BOARD_AVB_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+BOARD_AVB_ENABLE := false
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 262144
@@ -122,49 +125,28 @@ TARGET_USES_MKE2FS := true
 TW_EXCLUDE_TWRPAPP := true
 TW_OZIP_DECRYPT_KEY := "1c4c1ea3a12531ae491b21bb31613c11"
 #TW_SKIP_COMPATIBILITY_CHECK := true
+TW_NO_HAPTICS := true
 
 # Hack: prevent anti rollback
 PLATFORM_VERSION := 16.1.0
 PLATFORM_SECURITY_PATCH := 2099-12-31
-
-#SHRP specific flags
-
+#SHRP flags
 # Maintainer name
 SHRP_MAINTAINER := kanged99
-
 # Device codename
 SHRP_DEVICE_CODE := RMX1851
-
 # put this 0 if device has no EDL mode
 SHRP_EDL_MODE := 1
-
 SHRP_EXTERNAL := /sdcard1
 SHRP_INTERNAL := /sdcard
 SHRP_OTG := /usb_otg
-
 # Put 0 to disable flashlight
 SHRP_FLASH := 1
-
 # These are led paths, find yours then put here
 #SHRP_FONP_1 := "/sys/devices/platform/soc/c440000.qcom,spmi/spmi-0/spmi0-03/c440$3/c440000.qcom,spmi:qcom,pm660l@3:qcom,leds@d300/leds/led:torch_0/brightness"
 #SHRP_FONP_2 := /sys/devices/platform/soc/c440000.qcom,spmi/spmi-0/spmi0-03/c440$3/c440000.qcom,spmi:qcom,pm660l@3:qcom,leds@d300/leds/led:torch_1/brightness
 #SHRP_FONP_3 := /sys/devices/platform/soc/c440000.qcom,spmi/spmi-0/spmi0-03/c440$3/c440000.qcom,spmi:qcom,pm660l@3:qcom,leds@d300/leds/led:switch_0/brightness
-
-# Max Brightness of LED (Optional)
+# Max Brightness of LED
 SHRP_FLASH_MAX_BRIGHTNESS := 200
-
-# Check your device's recovery path, dont use blindly
+#check your device's recovery path, dont use blindly
 SHRP_REC := /dev/block/bootdevice/by-name/recovery
-
-# Use this flag only if your device is A/B
-SHRP_AB := false
-
-# Recovery Type (It can be treble,normal,SAR) [Only for About Section]
-SHRP_REC_TYPE := SAR
-
-# Recovery Type (It can be A/B or A_only) [Only for About Section]
-SHRP_DEVICE_TYPE := A_Only
-
-# SHRP Padding Flag (Only for rounded corner devices.)
-SHRP_STATUSBAR_RIGHT_PADDING := 40
-SHRP_STATUSBAR_LEFT_PADDING := 40
